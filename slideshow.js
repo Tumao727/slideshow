fdac0a8713cb42ec2ef0a74c72276e8f5106c93e
tumao <tumao@protonmail.com>
2018-09-26 09:11:46 +0800

    adjust code structure

--- slideshow.js
+++ slideshow.js
@@ -1,20 +1,26 @@
+const nextIndex = (slide, offset) => {
+    // 找出当前 active 的元素
+    let activeElement = e('.active')
+    // 求出当前 active 元素的下标
+    let index = Number(activeElement.id.slice(-1))
+    let numberOfImgs = Number(slide.dataset.img)
+    // 求出下一张图片的下标，从而通过选择器选出下一张图片
+    let nextIndex = (numberOfImgs + index + offset) % numberOfImgs
+    return nextIndex
+}
+
 const bindEventSlide = () => {
     let selector = ".slide-button" 
     bindAll(selector, 'click', (event) => {
         let self = event.target
         // 找到 slide 所在的 div
         let slide = self.closest('.slide-container')
-        // 找出当前 active 的元素
-        let activeElement = e('.active')
-        // 求出当前 active 元素的下标
-        let index = Number(activeElement.id.slice(-1))
-        let numberOfImgs = Number(slide.dataset.img)
         // 上一张 offset 是 -1
         // 下一张 offset 是 1
         let offset = Number(self.dataset.offset)
-        // 求出下一张图片的下标，从而通过选择器选出下一张图片
-        let nextIndex = (numberOfImgs + index + offset) % numberOfImgs
-        showImageAtIndex(nextIndex)
+        let index = nextIndex(slide, offset)
+        showImageAtIndex(index)
+        showIndicatorAtIndex(index)
     })
 }
 
@@ -24,6 +30,7 @@ const bindEventIndicator = () => {
         let self = event.target
         let index = Number(self.dataset.index)
         showImageAtIndex(index)
+        showIndicatorAtIndex(index)
     })
 }
 
@@ -36,7 +43,10 @@ const showImageAtIndex = (index) => {
     let img = e(nextSelector)
     // 为下一张图片添加 active class
     img.classList.toggle('active')
+}
 
+const showIndicatorAtIndex = (index) => {
+    let nextIndex = index
     // 切换小圆点
     let indicatorClass = 'indi-active'
     removeClassAll(indicatorClass)
